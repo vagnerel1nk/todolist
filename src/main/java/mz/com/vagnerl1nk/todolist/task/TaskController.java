625a759f0f8388cd4855c1119310ef3ab121f720
vagnerel1nk <vagnerel1nk@gmail.com>
2023-10-13 14:50:15 +0200

    Adicionamos a camada erros para

--- src/main/java/mz/com/vagnerl1nk/todolist/task/TaskController.java
+++ src/main/java/mz/com/vagnerl1nk/todolist/task/TaskController.java
@@ -65,14 +65,28 @@ public class TaskController {
     
 
     @PutMapping("/{id}")
-    public TaskModel  update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-
+    public ResponseEntity  update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
-        Utils.copyNonNullProperties(taskModel, task);
 
+
+       if (task == null) {
+           
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task not Founded");
     
-       return this.taskRepository.save(task);
+
+       }
+        
+       var idUser =  request.getAttribute("idUser");
+       if (!task.getIdUser().equals(idUser)) {
+        
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(" You dont have permission to change this task");
+       } 
+
+        
+       
+       Utils.copyNonNullProperties(taskModel, task);
+       var taskUpdated = this.taskRepository.save(task);
+       return ResponseEntity.ok().body(taskUpdated);
     }
 }
  
--- src/main/java/mz/com/vagnerl1nk/todolist/task/TaskModel.java
+++ src/main/java/mz/com/vagnerl1nk/todolist/task/TaskModel.java
@@ -40,6 +40,14 @@ public class TaskModel {
 
      @CreationTimestamp
      private LocalDateTime createdAt;
+
+
+     public void setTitle(String title) throws Exception {
+          if(title.length() > 50) {
+               throw new Exception("Title camp need 50 character"); 
+          }
+          this.title = title;
+     }
      
      
 
